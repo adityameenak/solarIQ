d114c7764f59664f6e9d18512867c3651a234200
Aditya Meenakshisundaram <63832850+adityameenak@users.noreply.github.com>
2025-05-19 20:39:31 -0500

    Add files via upload

--- newproj.py
+++ newproj.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 import requests
 from datetime import date
 
+st.markdown("""
+    <head>
+        <meta property="og:image" content="https://iqsolar.streamlit.app/?/logo.png">
+        <meta property="og:title" content="SolarIQ>
+        <meta property="og:description" content="Estimate solar performance and carbon impact by location.">
+    </head>
+""", unsafe_allow_html=True)
+
 
 #api keys
 API_KEY_NREL = st.secrets["API_KEY_NREL"]
